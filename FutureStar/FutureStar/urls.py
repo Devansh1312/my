07b941d111190d0047a543963524e7d4b9e2ec9a
soham1027 <sohamghayal02@gmail.com>
2024-11-07 12:35:08 +0530

    add url of TournamentApp

--- FutureStar/FutureStar/urls.py
+++ FutureStar/FutureStar/urls.py
@@ -28,6 +28,8 @@ urlpatterns = [
     path('', include('FutureStarAPI.urls')),  # Point to the app's API urls
     path('', include('FutureStarTeamApp.urls')),  # Point to the app's API urls
     path('', include('FutureStarTrainingGroupApp.urls')),  # Point to the app's API urls
+    path('', include('FutureStarTournamentApp.urls')),  # Point to the app's API urls
+
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ] 
