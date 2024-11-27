9c391855de019d46258f4a3e3ec5b3f4e5ed8732
Devansh1312 <shahdevansh13122002@gmail.com>
2024-11-27 16:49:16 +0530

    Changes

--- FutureStar/FutureStarTournamentApp/serializers.py
+++ FutureStar/FutureStarTournamentApp/serializers.py
@@ -266,14 +266,12 @@ class TournamentGamesHead2HeadSerializer(serializers.ModelSerializer):
     team_a_name = serializers.SerializerMethodField()
     team_b_name = serializers.SerializerMethodField()
     game_field_name = serializers.SerializerMethodField()
-    team_a_position = serializers.SerializerMethodField()
-    team_b_position = serializers.SerializerMethodField()
-
+   
     class Meta:
         model = TournamentGames
         fields = ['id', 'team_a_name', 'team_b_name', 'team_a_goal', 'team_b_goal', 
-                  'game_field_name', 'game_date', 'team_a_logo', 'team_b_logo',
-                  'team_a_position', 'team_b_position']
+                  'game_field_name', 'game_date', 'team_a_logo', 'team_b_logo'
+                ]
 
     def get_team_a_logo(self, obj):
         # Access the logo for team A through select_related to avoid extra queries
@@ -297,16 +295,6 @@ class TournamentGamesHead2HeadSerializer(serializers.ModelSerializer):
 
     def get_game_field_name(self, obj):
         return obj.game_field_id.field_name if obj.game_field_id else None
-
-    def get_team_a_position(self, obj):
-        # Fetch the position of team A from context
-        team_positions = self.context.get('team_positions', {})
-        return team_positions.get(obj.team_a.id, None) if obj.team_a else None
-
-    def get_team_b_position(self, obj):
-        # Fetch the position of team B from context
-        team_positions = self.context.get('team_positions', {})
-        return team_positions.get(obj.team_b.id, None) if obj.team_b else None
     
 
 class TournamentGameSerializer(serializers.ModelSerializer):
