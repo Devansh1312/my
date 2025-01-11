33d51f51fad404d90c853c55415e0d03cfd12724
soham1027 <sohamghayal02@gmail.com>
2025-01-11 15:31:23 +0530

    changes

--- FutureStar/FutureStar_App/admin.py
+++ FutureStar/FutureStar_App/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
+from FutureStar_App.models import *
+from FutureStarTeamApp.models import *
+from FutureStarAPI.models import *
+from FutureStarTournamentApp.models import *
+from urllib.parse import urlparse
+from FutureStarAPI.models import *
+from django.core.files.images import get_image_dimensions
+from FutureStarGameSystem.models import *
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Country)
+admin.site.register(City)
