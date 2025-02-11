f5465613c5c0c5183605d3622a23af56d4c5b401
Devansh1312 <shahdevansh13122002@gmail.com>
2025-02-11 18:58:14 +0530

    Remove current date and time from notification response data

--- FutureStar/FutureStarAPI/views.py
+++ FutureStar/FutureStarAPI/views.py
@@ -5410,12 +5410,9 @@ class CheckTrainingTimeAndSendNotificationsAPIView(APIView):
                     self.send_notification(team_founder, attendance_message, comments_message, push_data)
                     notifications_sent += 1
 
-        data = {"current_date_time": datetime.now().strftime("%Y-%m-%d %H:%M:%S")}
         response_data = {
             "message": "Notifications sent to {} users.".format(notifications_sent),
-            "data": data,
-            "notifiction_sent_to":self.send_notification,
-        }
+            }
 
         return Response(response_data, status=status.HTTP_200_OK)
     
